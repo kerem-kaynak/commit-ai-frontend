f8c3184e93111e4a23d2c99dbdd99ba4ac459af1
keremakaynak <kerem.kaynak@code.berlin>
2023-03-08 14:44:04 +0100

    feat:navbar

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,15 @@ import './assets/tailwind.css'
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div class="fixed top-0 left-0 right-0 bottom-0 bg-stone-50 p-16 flex justify-center font-mono">
+    <nav id="navbar" class="px-8 py-2 border-2 border-slate-500 rounded-2xl h-fit shadow-lg shadow-stone-300">
+      <ul class="flex flex-row gap-8">
+        <li class="transition-transform hover:scale-110 hover:ease-in"><RouterLink to="/">Tab 1</RouterLink></li>
+        <li class="transition-transform hover:scale-110 hover:ease-in"><RouterLink to="/">Tab 2</RouterLink></li>
+        <li class="transition-transform hover:scale-110 hover:ease-in"><RouterLink to="/">My Super Favorite Tab Tab Tab 3</RouterLink></li>
+      </ul>
+    </nav>
+  </div>
 
   <RouterView />
 </template>
